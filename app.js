2759dea5084820929f3b5248552306d891403442
AbelBerhanu <abelberhanu2018@gmail.com>
2025-11-19 09:20:02 +0300

    adding navbar css

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 // setup date
-const date = document.getElementById("date");
-date.innerHTML = new Date().getFullYear();
-console.log(date);
+const date = document.getElementById('date')
+
+date.innerHTML = new Date().getFullYear()
+// console.log(date);
+
+// setup navbar
+
+const navBtn = document.getElementById('nav-btn')
+const navbar = document.getElementById('navbar')
+const navClose = document.getElementById('nav-close')
+
+// console.log(navBtn)
+// console.log(navbar)
+// console.log(navClose)
+
+navBtn.addEventListener('click', () => {
+  navbar.classList.add('showNav')
+//   console.log(navbar)
+})
+
+navClose.addEventListener('click', () => {
+  navbar.classList.remove('showNav')
+//   console.log(navbar)
+})
